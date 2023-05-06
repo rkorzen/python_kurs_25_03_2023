a22a157b290a146172d5d880811199418eaac4bb
rkorzen <korzeniewski@gmail.com>
2023-05-06 12:57:35 +0200

    solve

--- aplikacje_webowe/django_applications/first_django_project/library/utils.py
+++ aplikacje_webowe/django_applications/first_django_project/library/utils.py
@@ -1,3 +1,23 @@
+import csv
+
+from library.models import Author, Book
+import os
+
+print(os.getcwd())
+
 def hello_django():
     print("Hello Django!")
 
+
+def create_author(row):
+    name, surname, birth_year, death_year = row
+    author = Author.objects.create(
+        name=name, surname=surname, birth_year=birth_year, death_year=death_year
+    )
+    return author
+
+def import_authors():
+    with open("data/authors.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            create_author(row)

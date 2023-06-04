c4b6f8dce8188acfea4ae53b690a3f1c2a3f4596
rkorzen <korzeniewski@gmail.com>
2023-06-04 15:22:19 +0200

    widoki generyczne

--- aplikacje_webowe/django_applications/examples/books/urls.py
+++ aplikacje_webowe/django_applications/examples/books/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import books_list, book_details, form_view
+from django.views.generic import TemplateView
+from .views import books_list, book_details, form_view, HomeTemplateView, BookListView, BookCreateView, BookDetailView
 
 app_name = "books" # przestrzeń nazw aplikacji
 urlpatterns = [
-    path("books/", books_list, name="list"),
-    path("books/<int:pk>/", book_details, name="details"),
+    path("", HomeTemplateView.as_view(), name="home"),
+    path("books/", BookListView.as_view(), name="list"),
+    path("books/create", BookCreateView.as_view(), name="create"),
+    path("books/<int:pk>/", BookDetailView.as_view(), name="details"),
     path("formview/", form_view, name="formview")
+
 ]
--- aplikacje_webowe/django_applications/examples/books/views.py
+++ aplikacje_webowe/django_applications/examples/books/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, CreateView, DetailView
 from books.models import Book
+from django.urls import reverse_lazy
+
 # Create your views here.
 from .forms import BookForm, BookForm2, MyForm, MyFormSet
 
+class HomeTemplateView(TemplateView):
+    template_name = "books/base.html"
 def books_list(request):
     books = Book.objects.all()
 
@@ -15,7 +20,22 @@ def books_list(request):
 
     return render(request, "books/list.html", {"books": books, "form": form})
 
+class BookListView(ListView):
+    template_name = "books/list.html"
+    model = Book
+    context_object_name = "books"
 
+
+class BookCreateView(CreateView):
+    template_name = "books/list.html"
+    model = Book
+    form_class = BookForm2
+    success_url = reverse_lazy("books:list")
+
+class BookDetailView(DetailView):
+    template_name = "books/details.html"
+    model = Book
+    context_object_name = "book"
 
 def book_details(request, pk):
     book = Book.objects.get(pk=pk)

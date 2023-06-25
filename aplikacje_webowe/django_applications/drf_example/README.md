88d48d841b98d0b8e5dcd807006f2f5140b24f4a
rkorzen <korzeniewski@gmail.com>
2023-06-25 16:29:27 +0200

    widoki funkcyjne

---
+++ aplikacje_webowe/django_applications/drf_example/README.md
@@ -0,0 +1,14 @@
+Czym jest REST?
+================
+
+REST (Representational State Transfer) to styl architektury używany w projektowaniu aplikacji internetowych. REST opiera się na kilku prostych zasadach, które pozwalają na komunikację i wymianę danych między różnymi komponentami systemu.
+
+REST definiuje zasoby (resources), które są reprezentowane przez unikalne adresy URL. Można to porównać do stron internetowych, gdzie każda strona ma swój unikalny adres. Na przykład, jeśli chcesz uzyskać informacje o książce, jej zasób może być reprezentowany przez adres URL "example.com/book".
+
+REST wykorzystuje standardowe metody protokołu HTTP, takie jak GET, POST, PUT i DELETE, aby manipulować tymi zasobami. Metoda GET służy do pobierania informacji o zasobie, POST do tworzenia nowego zasobu, PUT do aktualizacji istniejącego zasobu, a DELETE do usuwania zasobu.
+
+Kiedy klient wysyła żądanie do serwera przy użyciu odpowiedniej metody HTTP, serwer odpowiada zwracając dane w formacie zrozumiałym dla klienta, na przykład w formacie JSON lub XML. Dzięki temu klient może łatwo odczytać i przetwarzać te dane.
+
+REST jest elastycznym i skalowalnym sposobem projektowania aplikacji internetowych, ponieważ oddziela przetwarzanie danych od interfejsu użytkownika. Oznacza to, że można zmieniać i rozwijać serwery i klientów niezależnie od siebie, o ile są zgodne z ustalonymi zasadami REST.
+
+W skrócie, REST to sposób projektowania aplikacji internetowych, który opiera się na zasobach, adresach URL i standardowych metodach HTTP do komunikacji między klientem a serwerem.
--- aplikacje_webowe/django_applications/drf_example/snippets/views.py
+++ aplikacje_webowe/django_applications/drf_example/snippets/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+@api_view(['GET', 'POST'])
+def snippet_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=204)
